ac2c3e28d1333daabea9d88023b70e18352f378a
hakanyavuz <hakanyavuz@trex.com.tr>
2022-02-15 00:12:20 +0300

    Application katmanı projeye eklendi.

---
+++ TrainReservation.Application/IServiceReservation.cs
@@ -0,0 +1,10 @@
+﻿using TrainReservation.Domain.RequestObjects;
+using TrainReservation.Domain.ResponseObject;
+
+namespace TrainReservation.Application
+{
+    public interface IServiceReservation
+    {
+        ResponseReservation CheckReservation(RequestReservation requestReservation);
+    }
+}
